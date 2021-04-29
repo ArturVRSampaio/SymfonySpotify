3593101827033fd73adb3bcb14b2a1754674e715
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-29 10:49:36 -0300

    correcao user role

--- src/src/Controller/UserController.php
+++ src/src/Controller/UserController.php
@@ -29,6 +29,7 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRoles(["ROLE_ADMIN"]);
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
--- src/src/Form/UserType.php
+++ src/src/Form/UserType.php
@@ -13,7 +13,7 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('roles')
+            //->add('roles')
             ->add('password')
             ->add('birthDate')
             ->add('plan')
